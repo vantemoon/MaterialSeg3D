4d238edf10748f619d6df9b5146b0c3b8d91e1df
Whisper-MMM <sylviassunrx0820@outlook.com>
2025-02-16 22:39:35 +0800

    Update port

--- api.py
+++ api.py
@@ -386,4 +386,4 @@ def download_mesh_endpoint():
 # ------------------
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=7860, debug=True)
