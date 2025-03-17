472780fbc03aa663a1e1d630e0dfe24347c11e87
Whisper-MMM <sylviassunrx0820@outlook.com>
2025-03-17 10:57:03 +0800

    Change number of workers

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -5,4 +5,4 @@ conda activate materialseg3d-env
 export USE_CUDA=1
 
 cd MaterialSeg3D
-exec gunicorn --workers=4 --bind=0.0.0.0:8080 --timeout 1800 api:app
+exec gunicorn --workers=1 --bind=0.0.0.0:8080 --timeout 1800 api:app
